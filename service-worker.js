c5b35581c95e4d9d0a2a67a2c94d3066fda0338c
dayerling <cardoza05012005@gmail.com>
2025-07-25 11:02:56 -0600

    PWA

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'social-links-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './images/anime.jfif',
+  './images/favicon-32x32.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
