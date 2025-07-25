963bd1dd8b7512e78df6933cb70cb012bd85584e
dayerling <cardoza05012005@gmail.com>
2025-07-25 11:20:41 -0600

    PWA

--- service-worker.js
+++ service-worker.js
@@ -17,20 +17,22 @@ self.addEventListener('install', event => {
   );
 });
 
+
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+          return response || fetch(event.request);
+      })
   );
 });
