a9fd96f45bf9974ed6ec0cd8bd8ea1e9e8f17660
dayerling <cardoza05012005@gmail.com>
2025-07-25 11:25:03 -0600

    PWA

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'social-links-cache-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './images/anime.jfif',
-  './images/favicon-32x32.png',
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/images/anime.jfif',
+  '/images/favicon-32x32.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css',
   'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'
 ];
